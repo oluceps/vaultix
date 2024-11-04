9579c0ad1d41593b94241a24b11ea2e711f76ef0
oluceps <secinia@outlook.com>
2024-11-04 18:42:24 +0800

    +

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ mod cmd;
 mod helper;
 mod interop;
 mod profile;
+use std::os::unix::process::parent_id;
 
 fn main() -> Result<()> {
     let _ = spdlog::init_env_level();
-    for sink in spdlog::default_logger().sinks() {
-        sink.set_formatter(Box::new(PatternFormatter::new(pattern!(
-            "{^{level}} - {payload}{eol}" // remove datetime
-        ))))
+    let as_sd_unit = parent_id() == 1;
+    if as_sd_unit {
+        for sink in spdlog::default_logger().sinks() {
+            sink.set_formatter(Box::new(PatternFormatter::new(pattern!(
+                "{^{level}} - {payload}{eol}" // remove datetime
+            ))))
+        }
+        spdlog::debug!("Detected running as systemd unit");
     }
     let args: Args = argh::from_env();
     args.ayaya()
