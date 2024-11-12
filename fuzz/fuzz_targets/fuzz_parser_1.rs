38e169c40e7298202aa03053e62c603eea27144f
oluceps <secinia@outlook.com>
2024-11-13 04:44:01 +0800

    + octal parser fuzz

--- fuzz/fuzz_targets/fuzz_parser_1.rs
+++ fuzz/fuzz_targets/fuzz_parser_1.rs
@@ -1,6 +1,7 @@
 #![no_main]
 use arbitrary::Arbitrary;
 use lib::extract_all_hashes;
+use lib::parse_octal_str;
 use libfuzzer_sys;
 
 #[derive(Arbitrary, Debug)]
@@ -9,4 +10,5 @@ struct FuzzInput(String);
 libfuzzer_sys::fuzz_target!(|input: FuzzInput| {
     let mut v = Vec::new();
     let _ = extract_all_hashes(input.0.as_str(), &mut v);
+    let _ = parse_octal_str(input.0.as_str());
 });
